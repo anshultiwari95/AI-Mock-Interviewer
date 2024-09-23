d9584556a56d89dee6e6d34775b7938b106af550
anshultiwari95 <anshultiwari95@gmail.com>
2024-09-24 01:00:17 +0530

    Edit SignIn page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -7,8 +7,8 @@ export default function Page() {
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
           <img
             alt=""
-            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            className="absolute inset-0 h-full w-full object-cover opacity-80"
+            src="https://www.dice.com/binaries/medium/content/gallery/dice/insights/2022/08/shutterstock_510170158.jpg"
+            className="absolute inset-0 h-full w-full object-cover opacity-80 blur-sm"
           />
 
           <div className="hidden lg:relative lg:block lg:p-12">
@@ -31,9 +31,9 @@ export default function Page() {
               Welcome to AI Mock Interviewer 🦑
             </h2>
 
-            <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              nam dolorum aliquam, quibusdam aperiam voluptatum.
+            <p className="mt-4 leading-relaxed text-white">
+              Practice job interview questions tailored to your job description.
+              Get instant AI feedback to improve your answers.
             </p>
           </div>
         </section>
